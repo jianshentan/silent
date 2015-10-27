b789e20a834535314109979c0eb393133a048199
JS <jianshentan@gmail.com>
2015-10-26 23:32:13 -0700

    switch ssl port back

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ var development = {
 
 var production = {
   secret: 'hellovelocity',
-  redisClient: redis.createClient( 6380, 'silent.redis.cache.windows.net', {auth_pass: 'yi2f1yelyDAKO3Cys6dSDyFRZ6n+W1kG3phGFOLmmGU=' })
+  redisClient: redis.createClient( 6379, 'silent.redis.cache.windows.net', {auth_pass: 'yi2f1yelyDAKO3Cys6dSDyFRZ6n+W1kG3phGFOLmmGU=' })
 };
 
 module.exports = process.env.NODE_ENV == 'production' ? production : development;
